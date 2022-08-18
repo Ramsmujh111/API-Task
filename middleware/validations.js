5b5d9b436f3a4a7c4e30a21b94c8a4ca46c54f19
Ramsmujh <dev.bit.ram@gamil.com>
2022-08-18 15:51:33 +0530

    adding middleware to check verify user

--- middleware/validations.js
+++ middleware/validations.js
@@ -3,10 +3,9 @@ const Joi = require('joi');
 const validationSchema = Joi.object({
   userName: Joi.string().min(3).max(40).required(),
   password: Joi.string().pattern(/^[a-zA-Z0-9]{4,50}/),
-  confirm_password: Joi.ref("password"),
   email: Joi.string().email({
     minDomainSegments: 2,
-    tlds: { allow: ["com", "net", "yehoo"] },
+    tlds: { allow: ["com", "net", "yahoo"] },
   }),
   isAdmin: Joi.boolean().default(false),
   isVerifiead: Joi.boolean().default(false),
--- middleware/verifications.js
+++ middleware/verifications.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const logger = require("../config/winston");
+const user = require("../models/user");
 
 //verify web token
 const verifyToken = (req, res, next) => {
@@ -26,18 +27,32 @@ const verifyToken = (req, res, next) => {
 
 // verified User and authorize user is verified:
 const verifyUsers = async (req, res, next) => {
-  verifyToken(req, res, async () => {
-    const user = await User.findOne({ _id: req.user._id });
-    if (req.user.isVerifiead === user.isVerifiead) {
-      next();
-    } else {
-      logger.error(`Unauthorized`);
+  try {
+    const email = req.body.email;
+    const user = await User.findOne({ email: email });
+    if(!user){
+      logger.error(`Enter the valid email`);
       res.status(401).json({
+        status:false,
+        message:`Enter valid email to continue..`
+      })
+    }
+    if (user.isVerifiead) {
+      return next();
+    } else {
+      logger.error(`You're not verified user`);
+      res.status(400).json({
         status: false,
-        message: "Unauthorized",
+        message: `You're not verified user`,
       });
     }
-  });
+  } catch (error) {
+    logger.error(error.message);
+    res.status(400).json({
+      status:false,
+      message:error.message
+    })
+  }
 };
 
 // this token is authenticate the admin
