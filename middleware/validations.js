69c5a57b543e1e584e59c245ef0c6e54a97ca3a7
Ramsmujh <dev.bit.ram@gamil.com>
2022-08-18 14:11:20 +0530

    mongoose schema change

--- middleware/validations.js
+++ middleware/validations.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const validationSchema = Joi.object({
-  userName: Joi.string().alphanum().min(3).max(40).required(),
+  userName: Joi.string().min(3).max(40).required(),
   password: Joi.string().pattern(/^[a-zA-Z0-9]{4,50}/),
   confirm_password: Joi.ref("password"),
   email: Joi.string().email({
