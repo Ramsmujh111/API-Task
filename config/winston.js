517050b282c3d4be01e1f1a5bb3dab7e4e27089b
Ramsmujh <dev.bit.ram@gamil.com>
2022-08-22 10:57:58 +0530

    add comment

--- config/winston.js
+++ config/winston.js
@@ -1,11 +1,25 @@
 const winston = require("winston");
-
+/**
+ * @description my formate show log info
+ * @param {level} level create the log description like error , info ,debug etc,
+ * @param {message} message show the message like which type of message occurs
+ * @param {timestamp} timestamp is basically show the logs time and data
+ */
 const myformate = winston.format.printf(({ level, message, timestamp }) => {
   return `${timestamp} | [  ${level}  ] | ${message}`;
 });
-// new Date
-const date = new Date()
 
+/**
+ * @description create a date
+ * @param {date} 
+ */
+const date = new Date()
+/**
+ * @description create logger info
+ * @param {level} level it option you can also put here or put with the transports
+ * @param {format} format extract from the my formate
+ * @param {transports} transports create the logs file and store the logs info 
+ */
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
